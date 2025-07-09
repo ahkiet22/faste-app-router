d2abd765b35c7f5c342c9bee505f7a2bcd4b82d5
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-07-09 22:41:48 +0700

    chore: rename index.test.ts to example.test.ts

--- src/utils/__tests__/example.test.ts
+++ src/utils/__tests__/example.test.ts
@@ -1,5 +1,27 @@
 import { describe } from '@jest/globals'
 
+
+// describe('sum module', () => {
+//   test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).not.toBe(4)
+//   })
+
+//   test('compare object', () => {
+//     const obj: any = { name: 1 }
+//     obj['value'] = 2
+//     expect(obj).toEqual({ name: 1, value: 2 })
+//   })
+
+//   test('compare null', () => {
+//     const abc = null
+//     expect(abc).toBeNull() // expect(abc).toBe(null)
+//     expect(abc).toBeDefined() // expect(abc).toBe(null)
+//     expect(abc).toBeUndefined() // expect(abc).toBe(null)
+//     expect(abc).toBeTruthy() // expect(abc).toBe(null)
+//     expect(abc).toBeFalsy() // expect(abc).toBe(null)
+//   })
+// })
+
 // describe('number matchers', () => {
 //   const value = 3
 //   test('compare', () => {
